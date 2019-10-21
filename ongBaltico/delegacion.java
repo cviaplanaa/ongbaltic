ed553810c7a93ec029055fba9ce2e73743ea8bc1
cviaplanaa <56766840+cviaplanaa@users.noreply.github.com>
2019-10-21 20:51:09 +0200

    Delete delegacion.java

--- ongBaltico/delegacion.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class delegacion {
-	private String nombre;
-	private String direccion;
-	private int telefono;
-	private String ciudad;
-	private ArrayList<personal> personal;
-}
